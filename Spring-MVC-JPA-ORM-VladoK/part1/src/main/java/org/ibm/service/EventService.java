d0f7a75a5a82a962fdf8815e5a06f3d946d78f60
verhre7583 <104501528+verhre7583@users.noreply.github.com>
2023-06-28 12:40:25 +0200

    Add files via upload

---
+++ Spring-MVC-JPA-ORM-VladoK/part1/src/main/java/org/ibm/service/EventService.java
@@ -0,0 +1,27 @@
+package org.ibm.service;
+
+import org.ibm.model.Event;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EventService {
+
+  private List<Event> eventList = new ArrayList<>();
+
+  public EventService(){
+    Event event = new Event();
+    event.setPlace("ibm");
+    event.setPeopleCount(0);
+    eventList.add(event);
+  }
+  public List<Event> getEventList() {
+    return eventList;
+  }
+
+  public void addEvent(Event event){
+    eventList.add(event);
+  }
+}
---
+++ spring-boot-server/README.md
@@ -0,0 +1,12 @@
+## Run Spring Boot application
+
+1. start Postgres in docker-compose or podman-compose first:
+```
+docker-compose up
+# alternatively, if using podman-compose
+# podman-compose up
+```
+2. start the app
+```
+mvn spring-boot:run
+```
